704ade7a8a31b554ccf973fbc6170378e1ad56f2
Deep Chaudhary <deepdeep.ac@gmail.com>
2017-01-12 05:24:14 -0800

    Java solutions added

---
+++ java/EulerSolution.java
@@ -0,0 +1,13 @@
+/* 
+ * Interface for Project Euler Java solutions
+ * 
+ */
+
+
+// This forces every solution class to implement a common interface,
+// which is helpful for unit testing like in the EulerTest implementation.
+public interface EulerSolution {
+	
+	public String run();
+	
+}
